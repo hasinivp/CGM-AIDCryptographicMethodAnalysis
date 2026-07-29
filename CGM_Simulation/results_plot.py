e75b498e4f344a002052467e6158df58a3aa1b11
Suhasini Prakash <hasinivp@outlook.com>
2026-07-29 13:58:46 -0500

    Updated plot format

--- CGM_Simulation/results_plot.py
+++ CGM_Simulation/results_plot.py
@@ -8,7 +8,8 @@ import numpy as np
 import pandas as pd
 
 filename = 'results.csv'
-dataframe = pd.read_csv(filename)
+df = pd.read_csv(filename)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--interactive", help="Display plots interactively", action="store_true") #Custom argument/flag to enable interactive viewing of plots
@@ -17,6 +18,8 @@ args = parser.parse_args()
 
 if not args.interactive:
     matplotlib.use('Agg')
+
+dataframe = df.groupby('Encryption_Algorithm')[['Average_Encryption_Time', 'Average_Decryption_Time', 'Peak_Protocol_Memory']].mean().reset_index()
 
 dataframe.set_index("Encryption_Algorithm", inplace=True)
 dataframe["Average_Encryption_Time"] *= 10000000
@@ -36,6 +39,8 @@ plt.savefig("TimeBenchmark.pdf")
 if args.interactive:
     plt.show()
 #Plot 2
+
+
 ax2 = dataframe[["Peak_Protocol_Memory"]].plot(kind="bar", figsize=(10,6), width=.8)
 
 ax2.set_ylabel("Memory Used (bytes)")
